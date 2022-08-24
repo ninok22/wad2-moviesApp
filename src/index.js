eafc9cf318eac79ac212de1d635af64b229d72ad
Nino Kelehsashvili <20074819@mail.wit.ie>
2022-08-24 23:33:00 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+// import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
+import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route path="/movies/:id" component={MoviePage} />
+//         <Route path="/" component={HomePage} />
+//         <Redirect from="*" to="/" />
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// };
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/movies/:id" component={MoviePage} />
-        <Route path="/" component={HomePage} />
-        <Redirect from="*" to="/" />
-      </Switch>
-    </BrowserRouter>
-  );
-};
+    return (
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies/favorites">Favorites</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+          <Route path="/movies/:id" component={MoviePage} />
+          <Route exact path="/" component={HomePage} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
+    );
+  };
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -34,6 +34,14 @@ const genreId = Number(genreFilter);
     else setGenreFilter(value);
   };
 
+  //adding a 'favorite' property to the movie object
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
 useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -65,7 +73,7 @@ useEffect(() => {
             genreFilter={genreFilter}
         />
         </Grid>
-        <MovieList movies={displayedMovies}></MovieList>
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
       </Grid>
     </Grid>
   );
