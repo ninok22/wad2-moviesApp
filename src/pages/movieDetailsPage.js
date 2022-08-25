77b27e1251d9154661c3eb4654b2d1c0f2752c02
Nino Kelehsashvili <20074819@mail.wit.ie>
2022-08-25 15:13:53 +0100

    Show movie review excerpts; Refactor Details page.

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,46 +1,12 @@
-// import React from "react";
-import React, {useState, useEffect}  from "react";
-import MovieHeader from "../components/headerMovie/";
-import MovieDetails from "../components/movieDetails/";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import { getMovie, getMovieImages } from "../api/tmdb-api";
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import MovieDetails from "../components/movieDetails";
+import PageTemplate from "../components/templateMoviePage";
+import { getMovie } from "../api/tmdb-api";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  gridList: {
-    width: 450,
-    height: "100vh",
-  },
-}));
-
-const MoviePage = (props) => {
-  const classes = useStyles();
-  /*const movie = props.movie;
-  const images = props.images;*/ 
-  const { id } = props.match.params; //allows the component to extract the movie id from the browser's parameterized URL address.
+const MovieDetailsPage = (props) => {
+  const { id } = props.match.params;
   const [movie, setMovie] = useState(null);
-  const [images, setImages] = useState([]);
-
-  // //uses the API's movie endpoint to get the full details on the film.
-  // useEffect(() => {
-  //   fetch(
-  //     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-  //   )
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((movie) => {
-  //       // console.log(movie)
-  //       setMovie(movie);
-  //     });
-  // }, [id]);
 
   useEffect(() => {
     getMovie(id).then((movie) => {
@@ -48,67 +14,19 @@ const MoviePage = (props) => {
     });
   }, [id]);
 
-  // //gets the set of images for the same movie.
-  // useEffect(() => {
-  //   fetch(
-  //     `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((json) => json.posters)
-  //     .then((images) => {
-  //       // console,log(images)
-  //       setImages(images);
-  //     });
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
-  useEffect(() => {
-    getMovieImages(id).then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
       {movie ? (
         <>
-          <MovieHeader movie={movie} />
-          <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid item xs={3}>
-              <div className={classes.root}>
-                <GridList
-                  cellHeight={500}
-                  className={classes.gridList}
-                  cols={1}
-                >
-                  {images.map((image) => (
-                    <GridListTile
-                      key={image.file_path}
-                      className={classes.gridListTile}
-                      cols={1}
-                    >
-                      <img
-                        /*src={`https://image.tmdb.org/t/p/w500/${image}`}
-                        alt={image.poster_path}*/
-                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                        alt={image.file_path}
-                      />
-                    </GridListTile>
-                  ))}
-                </GridList>
-              </div>
-            </Grid>
-            <Grid item xs={9}>
-              <MovieDetails movie={movie} />
-            </Grid>
-          </Grid>
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie} />
+          </PageTemplate>
         </>
       ) : (
-        <h2>Waiting for API data</h2>
+        <p>Waiting for movie details</p>
       )}
     </>
   );
 };
 
-export default MoviePage;
+export default withRouter(MovieDetailsPage);
