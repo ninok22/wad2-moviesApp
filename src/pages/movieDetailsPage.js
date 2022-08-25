d001dd22f36d684526b1b44024e839b63b01c9e5
Nino Kelehsashvili <20074819@mail.wit.ie>
2022-08-25 16:29:51 +0100

    Refactor - useMovie custom hook.

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,18 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { withRouter } from "react-router-dom";
 import MovieDetails from "../components/movieDetails";
 import PageTemplate from "../components/templateMoviePage";
-import { getMovie } from "../api/tmdb-api";
+import useMovie from "../hooks/useMovie";
 
 const MovieDetailsPage = (props) => {
   const { id } = props.match.params;
-  const [movie, setMovie] = useState(null);
-
-  useEffect(() => {
-    getMovie(id).then((movie) => {
-      setMovie(movie);
-    });
-  }, [id]);
+  const [movie] = useMovie(id);
 
   return (
     <>
@@ -30,3 +24,37 @@ const MovieDetailsPage = (props) => {
 };
 
 export default withRouter(MovieDetailsPage);
+
+
+// import React, { useState, useEffect } from "react";
+// import { withRouter } from "react-router-dom";
+// import MovieDetails from "../components/movieDetails";
+// import PageTemplate from "../components/templateMoviePage";
+// import { getMovie } from "../api/tmdb-api";
+
+// const MovieDetailsPage = (props) => {
+//   const { id } = props.match.params;
+//   const [movie, setMovie] = useState(null);
+
+//   useEffect(() => {
+//     getMovie(id).then((movie) => {
+//       setMovie(movie);
+//     });
+//   }, [id]);
+
+//   return (
+//     <>
+//       {movie ? (
+//         <>
+//           <PageTemplate movie={movie}>
+//             <MovieDetails movie={movie} />
+//           </PageTemplate>
+//         </>
+//       ) : (
+//         <p>Waiting for movie details</p>
+//       )}
+//     </>
+//   );
+// };
+
+// export default withRouter(MovieDetailsPage);
